1f55c208d0a910e199614be7f7986844420234b5
Hussainarbab <arbabhussan63@gmail.com>
2025-11-19 16:17:50 +0500

    mode commit

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 
-  const btn = document.getElementById("toggleColorBtn");
-  let isBlue = false;
+//   const btn = document.getElementById("toggleColorBtn");
+//   let isBlue = false;
 
-  btn.addEventListener("click", () => {
-    document.body.style.backgroundColor = isBlue ? "white" : "black";
-    isBlue = !isBlue;
-  });
+//   btn.addEventListener("click", () => {
+//     document.body.style.backgroundColor = isBlue ? "white" : "black";
+//     isBlue = !isBlue;
+//   });
+
+ function darkMode() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+  }
+
+  function lightMode() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+  }
 
